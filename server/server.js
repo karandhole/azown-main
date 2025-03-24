a6d19483e1ab073638aaeb21d8bd80c54cd93a0d
karan dhole <karan.dhole20@pccoepune.org.>
2025-03-24 17:48:28 +0530

    S

--- server/server.js
+++ server/server.js
@@ -11,23 +11,11 @@ const port = 5000;
 app.use(express.json({ limit: "500mb" }));
 app.use(cors());
 
-// Correct `__dirname` Usage
+// Fixing __dirname properly
 const buildPath = path.join(__dirname, "../client/build");
+app.use(express.static(buildPath));
 
-// Ensure Build Folder Exists
-const fs = require("fs");
-if (!fs.existsSync(buildPath)) {
-  console.error(" React build folder not found:", buildPath);
-} else {
-  app.use(express.static(buildPath));
-
-  //  Serve React App
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(buildPath, "index.html"));
-  });
-}
-
-//  Available Routes
+// Available Routes
 app.use("/user", require("./routes/user"));
 app.use("/property", require("./routes/property"));
 app.use("/admin", require("./routes/admin"));
@@ -36,7 +24,12 @@ app.use("/leadprop", require("./routes/leadprop"));
 app.use("/handler", require("./routes/handler"));
 app.use("/likes", require("./routes/likeprop"));
 
-//  Start the Server
+// Serve frontend for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
+
+// Start the Server
 app.listen(port, () => {
   console.log(` Server is running on http://localhost:${port}`);
 });
